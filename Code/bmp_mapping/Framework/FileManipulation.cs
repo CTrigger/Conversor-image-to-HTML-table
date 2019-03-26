efa1e0dd098cd02c16104ce9c13b825e818b1194
CTrigger <hexadecimal.exe@gmail.com>
2019-03-26 00:58:25 -0300

    First Commit

---
+++ Code/bmp_mapping/Framework/FileManipulation.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FileManipulation.Framework
+{
+    class WriteFile
+    {
+        /*
+         * Starts the constructor class with the file content, file name and the file extension
+         * This will generate the Entire File
+         * After this release the memory, there will not be more use for it
+        */
+        public WriteFile(string[] FileContent, string FileName, string FileExtension)
+        {
+            using (System.IO.StreamWriter file =
+                       new System.IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + FileName + "." + FileExtension, true))
+            {
+                try
+                {
+                    for (int i = 0; i < FileContent.Length; i++)
+                    {
+                        file.WriteLine(FileContent[i]);
+                    }
+
+                }
+                catch (Exception error)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nOcorreu um erro, entre em contato com o desenvolvedor... (eu)\nNão esquecer do print por favor\n");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("fb.com/MartialBuda\n");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(error.Source + "\n");
+                    Console.WriteLine(error.Message + "\n");
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Clique em qualquer tecla para fechar...");
+                    Console.ReadKey();
+                }
+
+            }
+        }
+
+    }
+
+    class ReadFile
+    {
+        /*
+         * Start the constructor in same location where the file will be used
+         * the Atribute Text to made the read
+        */
+        public string[] Text;
+        public int totalLines;
+        public ReadFile(string FileName, string FileExtension)
+        {
+            using (StreamReader File = new StreamReader(FileName + "." + FileExtension))
+            {
+                try
+                {
+                    //while (!sr.EndOfStream)
+                    //{
+                    //    this.Text = sr.ReadLine();
+
+                    //}
+                    this.Text = File.ReadToEnd().Split('\n');
+                    this.totalLines = Text.Length;
+                }
+                catch (Exception error)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nOcorreu um erro, entre em contato com o desenvolvedor... (eu)\nNão esquecer do print por favor\n");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("fb.com/MartialBuda\n");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(error.Source + "\n");
+                    Console.WriteLine(error.Message + "\n");
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Clique em qualquer tecla para fechar...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+
+}
---
+++ Code/bmp_mapping/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using FileManipulation.Framework;
+
+namespace bmp_mapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arquivo = "";
+            string x;
+            Console.Write("Nome da imagem: ");
+            x = Console.ReadLine();
+            //x = "teste.bmp";
+            using (Bitmap bmp = new Bitmap(x))
+            {
+                Console.WriteLine("Largura: " + bmp.Width);
+                Console.WriteLine("Altura: " + bmp.Height);
+
+                arquivo += "<!DOCTYPE html>\n";
+                arquivo += "<html>\n";
+                arquivo += "<head>\n";
+                arquivo += "<style>\n";
+                arquivo += "td{\n";
+                //arquivo += "width: 1px;\n";
+                //arquivo += "height: 1px;\n";
+                arquivo += "width: 0.01px;\n";
+                arquivo += "height: 0.01px;\n";
+                arquivo += "}\n";
+                arquivo += "table, tr, td{\n";
+                arquivo += "border-spacing: 0px;\n";
+                arquivo += "}\n";
+                arquivo += "</style>\n";
+                arquivo += "</head>\n";
+                arquivo += "<body>\n";
+                arquivo += "</body>\n";
+                arquivo += "<table>\n";
+                for (int i = 0; i < bmp.Height; i++)
+                {
+                    arquivo += "    <tr>\n";
+                    for (int j = 0; j < bmp.Width; j++)
+                    {
+                        Color clr = bmp.GetPixel(j, i);
+                        int red = clr.R;
+                        int green = clr.G;
+                        int blue = clr.B;
+
+
+                        arquivo +=
+                            "       <td bgcolor=\"#" +
+                            Convert.ToString(red, 16).PadLeft(2, '0') +
+                            Convert.ToString(green, 16).PadLeft(2, '0') +
+                            Convert.ToString(blue, 16).PadLeft(2, '0') + "\">" +
+                            "</td>\n";
+
+                    }
+                    arquivo += "    </tr>\n";
+
+                }
+                arquivo += "</table>\n";
+                arquivo += "</body>\n";
+                arquivo += "</html>\n";
+
+
+            }
+            new WriteFile(arquivo.Split('\n'), x, "htm");
+            Console.WriteLine("Concluído");
+            Console.ReadKey();
+        }
+    }
+}
